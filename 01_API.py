5bfacc1ecd1b4cf7e4af7c91262da90e3128a55f
thiagodaversagit config --global user.email thiagdaversa@gmail.comgit config --global user.name thiagodaversa <thiagdaversa@gmail.co>
2026-06-25 23:33:28 -0300

    Inclusão da url da API da nasa

--- 01_API.py
+++ 01_API.py
@@ -1,6 +1,12 @@
 import requests
 
-resposta = requests.get("")
+url_nasa =  "https://api.nasa.gov/planetary/apod"
+params = {
+    "api_key": "DEMO_KEY"
+}
+
+#
+resposta = requests.get("url_nasa")
 
 if resposta.status_code == 200:
     print(f"Sucesso! {resposta.status_code}")
