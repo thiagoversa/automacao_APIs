b717cc46f501b1993b512f5ec674e0abde28998c
thiagodaversagit config --global user.email thiagdaversa@gmail.comgit config --global user.name thiagodaversa <thiagdaversa@gmail.co>
2026-07-01 18:22:16 -0300

    Commit erro

--- 01_API.py
+++ 01_API.py
@@ -9,7 +9,7 @@ params = {
 resposta = requests.get("url_nasa")
 
 if resposta.status_code == 200:
-    print(f"Sucesso! {resposta.status_code}")
+    print(f"Sucesso! {resposta.status_code}   erro grande")
 else:
     print(f"Erro! {resposta.status_code}")
 
